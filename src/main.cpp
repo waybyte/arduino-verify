fa48082172bbef47297fd71034654344f277cf52
Ajay Bhargav <ajay.bhargav@waybyte.in>
2023-04-12 01:18:43 +0530

    Add project files to verify build
    Signed-off-by: Ajay Bhargav <ajay.bhargav@waybyte.in>

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <Net.h>
+#include <Modemif.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
